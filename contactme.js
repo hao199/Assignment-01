6ca5168241b8199bc21301a45a4df25bd6752fd9
hao199 <haoze199@gmail.com>
2021-12-03 18:45:25 +0800

    add validations and css to contactpage

--- contactme.js
+++ contactme.js
@@ -6,3 +6,35 @@ function submitContact(){
 
     alert("Hi " + name.value + ". I will get back to you at " + email.value);
 }
+
+function validCheck(){
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var message = document.getElementById("message");
+    const validateEmail = (email) => {
+        return String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      };
+
+    if(name.value === "" || name.value == null){
+        alert("Name has not been entered!");
+        return;
+    }
+    if(email.value === "" || email.value == null){
+        alert("Email has not been entered!");
+        return;
+    }
+    if(!validateEmail(email.value)){
+        alert("Please enter a valid email!");
+        return;
+    }
+    if(message.value === "" || message.value == null){
+        alert("Message has not been entered!");
+        return;
+    }
+
+    submitContact();
+}
